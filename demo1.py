9e217079da65b0530462dca6a676302c3f83e5ac
ajay <ajayshaji201@gmaul.com>
2022-10-17 22:04:25 +0530

    random number

---
+++ demo1.py
@@ -0,0 +1,43 @@
+import random
+number= random.randint(0,6)
+if number == 1:
+    print(' __________')
+    print('|          |')
+    print('|     0    |')
+    print('|          |')
+    print(' __________')
+
+if number == 2:
+    print(' __________')
+    print('|          |')
+    print('| 0      0 |')
+    print('|          |')
+    print(' __________')
+
+if number == 3:
+    print(' __________')
+    print('|          |')
+    print('| 0   0   0|')
+    print('|          |')
+    print(' __________')
+
+if number == 4:
+    print(' __________')
+    print('|          |')
+    print('| 0  0   0 |')
+    print('|          |')
+    print(' __________')
+
+if number == 5:
+    print(' __________')
+    print('|     0     |')
+    print('|     0     |')
+    print('|     0     |')
+    print(' __________ |')
+
+if number == 6:
+    print(' __________')
+    print('|          |')
+    print('|          |')
+    print('|          |')
+    print(' __________ ')
